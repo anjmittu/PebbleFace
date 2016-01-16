7ff54645ac1b45901cb815515f13633657f614d0
anjmittu <soccerball910@yahoo.com>
2016-01-16 18:03:13 -0500

    added accelerometer

--- src/main.c
+++ src/main.c
@@ -1,35 +1,49 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_time_layer;
 
-static void tap_handler(AccelAxisType axis, int32_t direction) {
-  switch (axis) {
-  case ACCEL_AXIS_X:
-    if (direction > 0) {
-      APP_LOG(APP_LOG_LEVEL_INFO, "X axis positive.");
-    } else {
-      APP_LOG(APP_LOG_LEVEL_INFO, "X axis negative.");
-    }
-    break;
-  case ACCEL_AXIS_Y:
-    if (direction > 0) {
-      APP_LOG(APP_LOG_LEVEL_INFO, "Y axis positive.");
-    } else {
-      APP_LOG(APP_LOG_LEVEL_INFO, "Y axis negative.");
-    }
-    break;
-  case ACCEL_AXIS_Z:
-    if (direction > 0) {
-      APP_LOG(APP_LOG_LEVEL_INFO, "Z axis positive.");
-    } else {
-      APP_LOG(APP_LOG_LEVEL_INFO, "Z axis negative.");
-    }
-    break;
-  }
+static void data_handler(AccelData *data, uint32_t num_samples) {
+  // Long lived buffer
+  static char s_buffer[128];
+
+  // Compose string of all data for 3 samples
+  snprintf(s_buffer, sizeof(s_buffer), 
+    "N X,Y,Z\n0 %d,%d,%d\n",//1 %d,%d,%d\n2 %d,%d,%d", 
+    data[0].x, data[0].y, data[0].z//, 
+    //data[1].x, data[1].y, data[1].z, 
+    //data[2].x, data[2].y, data[2].z
+  );
+
+  //Show the data
+  APP_LOG(APP_LOG_LEVEL_INFO, s_buffer);
+}
+
+static void main_window_load(Window *window){
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
+	
+	s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+	
+	text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	
+	layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+	
+	
+}
+
+static void main_window_unload(Window *window){
+	text_layer_destroy(s_time_layer);
 }
 
 void init(){
-	accel_tap_service_subscribe(tap_handler);
+	uint32_t num_samples = 1;
+	accel_data_service_subscribe(num_samples, data_handler);
+
+	accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
 	
 	s_main_window = window_create();
   
@@ -37,6 +51,8 @@ void init(){
     .load = main_window_load,
     .unload = main_window_unload
   });
+	
+	window_stack_push(s_main_window, true);
 }
 
 void deinit(){
