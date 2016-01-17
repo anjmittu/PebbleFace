8160035fb4ce96c8e15a9c39790cf758586be887
anjmittu <soccerball910@yahoo.com>
2016-01-16 21:44:03 -0500

    accellerometer

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <pebble.h>
-
+#define THRESHOLDL 300
+#define THRESHOLDH 400
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+int prevY = 10000000;
 int prevX = 10000000;
+int prevZ = 10000000;
 bool started = false;
 int wait = 0;
 
@@ -12,27 +15,30 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
 		wait--;
 	}else{
 		// Long lived buffer
-		if (prevX == 10000000){
+		if (prevY == 10000000 && prevX == 10000000 && prevZ == 10000000){
+			prevY = data[0].y;
 			prevX = data[0].x;
+			prevZ = data[0].z;
 		}
 
-		else if (abs(prevX-data[0].x) > 400){
+		else if (abs(data[0].y-data[1].y) < THRESHOLDL && abs(data[1].y-data[2].y) > THRESHOLDH){
 			if(started){
 				started = false;
 				APP_LOG(APP_LOG_LEVEL_INFO, "end");
-				APP_LOG(APP_LOG_LEVEL_INFO, "%d", data[0].x);
+				//APP_LOG(APP_LOG_LEVEL_INFO, "%d\n%d\n", abs(data[0].y-data[1].y), abs(data[1].y-data[2].y));
+				vibes_short_pulse();
 			}else{
 				started = true;
 				APP_LOG(APP_LOG_LEVEL_INFO, "start");
-				APP_LOG(APP_LOG_LEVEL_INFO, "%d", data[0].x);
+				//APP_LOG(APP_LOG_LEVEL_INFO, "%d\n%d\n", abs(data[0].y-data[1].y), abs(data[1].y-data[2].y));
+				vibes_long_pulse();
 			}
+			prevY = data[0].y;
 			prevX = data[0].x;
-			wait = 10;
+			prevZ = data[0].z;
+			wait = 25;
 		}
 	}
-
-
-  //Show the data
 }
 
 static void main_window_load(Window *window){
@@ -56,10 +62,10 @@ static void main_window_unload(Window *window){
 }
 
 void init(){
-	uint32_t num_samples = 1;
+	uint32_t num_samples = 3;
 	accel_data_service_subscribe(num_samples, data_handler);
 
-	accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+	accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
 	
 	s_main_window = window_create();
   
